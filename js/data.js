917c24b5ee36755283b58de3a5265672828242af
Kenny Park <kennyparc@gmail.com>
2021-09-08 12:40:35 -0700

    Completed issue #1 feature.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,15 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', beforeUnload);
+
+function beforeUnload(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('data-model', dataJSON);
+}
+
+var previousDataJSON = localStorage.getItem('data-model');
+if (previousDataJSON) {
+  data = JSON.parse(previousDataJSON);
+}
--- js/main.js
+++ js/main.js
@@ -1,2 +1,34 @@
 /* global data */
 /* exported data */
+
+var photoURL = document.querySelector('#photo-url');
+var img = document.querySelector('img');
+var newEntryForm = document.querySelector('form');
+
+photoURL.addEventListener('input', photoURLUpdate);
+
+function photoURLUpdate(event) {
+  img.setAttribute('src', event.target.value);
+  if (img.getAttribute('src') === '') {
+    img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  }
+}
+
+newEntryForm.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+  var inputValues = {};
+  var titleValue = newEntryForm.elements.title.value;
+  var urlValue = newEntryForm.elements['photo-url'].value;
+  var notesvalue = newEntryForm.elements.notes.value;
+  inputValues.title = titleValue;
+  inputValues.url = urlValue;
+  inputValues.notes = notesvalue;
+  inputValues.nextEntryID = data.nextEntryID;
+  inputValues.entryId = data.nextEntryId;
+  data.entries.unshift(inputValues);
+  data.nextEntryId++;
+  document.querySelector('#form').reset();
+  img.setAttribute('src', 'images/placeholder-image-square.jpg');
+}
